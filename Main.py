3b46f01b17f274fb5a30ff1e656894a213e4d4f7
Steffan Antony <ashonantony256@gmail.com>
2026-08-19 22:54:40 +0530

    basic chat interface made

--- Main.py
+++ Main.py
@@ -1,1 +1,25 @@
+import openai
 
+client = openai.OpenAI(
+    base_url="http://poopy.local:8080/v1",
+    api_key="not-needed"
+)
+while True:
+    print(">>> ", end="", flush=True)
+    text = input()
+    if text=="/bye":
+        break
+    response = client.chat.completions.create(
+        model = "Qwen3-4B-Q4_K_M",
+        messages = [
+            {"role": "user", "content": text}
+        ],
+        stream = True
+    )
+
+    for chunk in response:
+        reply = chunk.choices[0].delta.content
+        if reply:
+            print(reply, end="", flush=True)
+
+    print()
