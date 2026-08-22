4c5919a2f4e282df1ccef5da100ba5a9c60f2b5d
Steffan Antony <ashonantony256@gmail.com>
2026-08-22 15:51:04 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,16 +1,24 @@
 import openai
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
-client = openai.OpenAI(
+local_client = openai.OpenAI(
     base_url="http://poopy.local:8080/v1",
     api_key="not-needed"
+)
+
+open_router_client = openai.OpenAI(
+    base_url = "https://openrouter.ai/api/v1",
+    api_key = os.getenv("OPEN_ROUTER_API_KEY")
 )
 while True:
     print(">>> ", end="", flush=True)
     text = input()
     if text=="/bye":
         break
-    response = client.chat.completions.create(
-        model = "Qwen3-4B-Q4_K_M",
+    response = open_router_client.chat.completions.create(
+        model = "qwen/qwen3-8b",
         messages = [
             {"role": "user", "content": text}
         ],
